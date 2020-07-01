2fc2a717ae95b054d3c343b2d5b3ea226373500e
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-01 18:43:06 +0100

    Update raffle.py

--- raffle/raffle.py
+++ raffle/raffle.py
@@ -91,7 +91,7 @@ class Raffle(BaseCog):
         embed.add_field(name="Winners", value=f'{winners}')
         role_info = f'{", ".join(str_roles) if roles else "@everyone"}'
         embed.add_field(name="Open to:", value=role_info)
-        embed.add_field(name="Ends:", value=f'{fmt_end}' UTC)
+        embed.add_field(name="Ends:", value=f'{fmt_end} UTC')
         embed.add_field(name="Raffle ID:", value=f'{msg.id})
         msg = await channel.send(embed=embed)
         embed.set_footer(text=(f'Winners: {winners} | '
