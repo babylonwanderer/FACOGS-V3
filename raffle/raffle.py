98e0879c31e35270112272aa6e3050acca49455e
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-01 19:19:43 +0100

    Update raffle.py

--- raffle/raffle.py
+++ raffle/raffle.py
@@ -85,7 +85,7 @@ class Raffle(BaseCog):
         except:
             color = await self.bot.get_embed_color(ctx)
             embed = discord.Embed(description=description, title=title, color=color) ### new code
-        embed.add_field(name="Winners", value=f'{winners}')
+        #embed.add_field(name="Winners", value=f'{winners}')
         role_info = f'{", ".join(str_roles) if roles else "@everyone"}'
         embed.add_field(name="Open to:", value=role_info)
         msg = await channel.send(embed=embed)
