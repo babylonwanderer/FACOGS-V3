85187ab91efa1bd8b7cdb0cc0f14dd1b3d5d4f41
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-04 02:35:18 +0100

    Update mycog.py

--- test/mycog.py
+++ test/mycog.py
@@ -4,7 +4,7 @@ class Stun(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def stun(self, ctx):
+    async def mycom(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
