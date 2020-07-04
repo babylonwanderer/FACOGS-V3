77ab4b6bf3bcda415553379454b197de1af0462d
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-04 02:11:28 +0100

    Update stun.py

--- stun/stun.py
+++ stun/stun.py
@@ -8,11 +8,3 @@ class stun(commands.Cog):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
-
-##    @commands.command()
-##    async def stun(self, ctx):
-##        """Stuns the mentioned user.. """
-
-##        #Your code will go here
-##        await ctx.send("takes aim at " + user.mention + ", and drops them to the deck with a Type II <:mkii:340431228970860544>")
-##        await ctx.send("Poor " + user.mention + " lays motionless for 5 mins.")
