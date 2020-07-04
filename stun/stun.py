db78d9bf00b435b13bf925ef67a550fc9373dabe
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-04 01:40:08 +0100

    Update stun.py

--- stun/stun.py
+++ stun/stun.py
@@ -4,9 +4,9 @@ class Stun:
     """Stuns the user mentioned """
 
     @commands.command()
-    async def stun(self, user : discord.Member):
+    async def stun(self, ctx):
         """Stuns the mentioned user.. """
 
         #Your code will go here
-        await self.bot.say("takes aim at " + user.mention + ", and drops them to the deck with a Type II <:mkii:340431228970860544>")
-        await self.bot.say("Poor " + user.mention + " lays motionless for 5 mins.")
+        await ctx.send("takes aim at " + user.mention + ", and drops them to the deck with a Type II <:mkii:340431228970860544>")
+        await ctx.send("Poor " + user.mention + " lays motionless for 5 mins.")
