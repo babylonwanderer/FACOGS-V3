f8330bf509a54e06cdffe194d61ba46f48f47ee1
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-04 01:49:53 +0100

    Update stun.py

--- stun/stun.py
+++ stun/stun.py
@@ -1,12 +1,18 @@
 from redbot.core import commands
 
-class Stun:
+class Stun(commands.Cog):
     """Stuns the user mentioned """
-
+    
     @commands.command()
-    async def stun(self, ctx):
-        """Stuns the mentioned user.. """
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
 
-        #Your code will go here
-        await ctx.send("takes aim at " + user.mention + ", and drops them to the deck with a Type II <:mkii:340431228970860544>")
-        await ctx.send("Poor " + user.mention + " lays motionless for 5 mins.")
+##    @commands.command()
+##    async def stun(self, ctx):
+##        """Stuns the mentioned user.. """
+##
+##        #Your code will go here
+##        await ctx.send("takes aim at " + user.mention + ", and drops them to the deck with a Type II <:mkii:340431228970860544>")
+##        await ctx.send("Poor " + user.mention + " lays motionless for 5 mins.")
