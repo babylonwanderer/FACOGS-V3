2a0ceb20909612472b4a87408b4fca8bd5bc8f3d
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-04 02:34:02 +0100

    Update mycog.py

--- test/mycog.py
+++ test/mycog.py
@@ -1,10 +1,10 @@
 from redbot.core import commands
 
-class Mycog(commands.Cog):
+class Stun(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def stun(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
